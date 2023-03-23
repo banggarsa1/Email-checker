d1a6dea60d4fb4efbd76230ba9ba2c058f8d5402
SANAGO <sanago3130@gmail.com>
2023-03-23 12:42:18 +0100

    First commit

---
+++ index_ended.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const { isEmail } = require('validator');
+const axios = require('axios');
+const apiKey = 'Ton Api key'; //https://hunter.io/ se faire un compte pour recup l'API key
+
+const fileContents = fs.readFileSync('email.txt', 'utf-8'); // Remplace "email.txt" par le nom de ton ficher avec les emails.
+const emailArray = fileContents.split('\n');
+
+const validEmails = [];
+const undeliverableEmails = [];
+const riskyEmails = [];
+const unknownEmails = [];
+const invalidEmails = [];
+
+emailArray.forEach(email => {
+  if (isEmail(email.trim())) {
+    verifyEmail(email.trim());
+  } else {
+    invalidEmails.push(email.trim());
+  }
+});
+
+function verifyEmail(email) {
+    axios.get(`https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`)
+    .then(response => {
+      const data = response.data.data;
+      if (data.result === 'deliverable') {
+        console.log(`\x1b[32mValid emails: ${email}`);
+        validEmails.push(email);
+        fs.appendFileSync('deliverable.txt', email + '\n');
+    } else if (data.result === 'undeliverable') {
+        undeliverableEmails.push(email);
+        fs.appendFileSync('undeliverable.txt', email + '\n');
+      } else if (data.result === 'risky') {
+        riskyEmails.push(email);
+        fs.appendFileSync('risky.txt', email + '\n');
+      } else {
+        unknownEmails.push(email);
+        fs.appendFileSync('unknown.txt', email + '\n');
+      }
+    })
+    .catch(error => {
+      console.log(error.response.data);
+  });
+}
+
+console.log(`\x1b[32mValid emails: ${validEmails.join('\n')}`);
