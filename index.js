b966bbd4b28ae276abe84173227ca8017a33cb00
SANAGO <sanago3130@gmail.com>
2023-03-23 13:04:16 +0100

    Update: you can now add a proxy list

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const { isEmail } = require('validator');
 const axios = require('axios');
-const apiKey = 'Ton Api key'; //https://hunter.io/ se faire un compte pour recup l'API key
+const axiosProxy = require('axios-proxy');
+const apiKey = 'ddb1a3eaf07f47376298cf5e69d7083afd0fd084'; //https://hunter.io/ se faire un compte pour recup l'API key
 
-const fileContents = fs.readFileSync('email.txt', 'utf-8'); // Remplace "email.txt" par le nom de ton ficher avec les emails.
+const fileContents = fs.readFileSync('emails.txt', 'utf-8'); // Remplace "email.txt" par le nom de ton ficher avec les emails.
 const emailArray = fileContents.split('\n');
 
 const validEmails = [];
@@ -11,6 +12,11 @@ const undeliverableEmails = [];
 const riskyEmails = [];
 const unknownEmails = [];
 const invalidEmails = [];
+
+function getRandomProxy() {
+  const proxies = fs.readFileSync('proxies.txt', 'utf-8').trim().split('\n'); // Ici le noms du fichiers qui contiens les proxies.
+  return proxies[Math.floor(Math.random() * proxies.length)];
+}
 
 emailArray.forEach(email => {
   if (isEmail(email.trim())) {
@@ -22,13 +28,21 @@ emailArray.forEach(email => {
 });
 
 function verifyEmail(email) {
-    axios.get(`https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`)
+  const proxy = getRandomProxy();
+  const instance = axios.create({
+    proxy: {
+      host: proxy.split(':')[0],
+      port: proxy.split(':')[1],
+    },
+  });
+
+  instance.get(`https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`)
     .then(response => {
       const data = response.data.data;
       if (data.result === 'deliverable') {
         validEmails.push(email);
         fs.appendFileSync('deliverable.txt', email + '\n');
-    } else if (data.result === 'undeliverable') {
+      } else if (data.result === 'undeliverable') {
         undeliverableEmails.push(email);
         fs.appendFileSync('undeliverable.txt', email + '\n');
       } else if (data.result === 'risky') {
@@ -41,7 +55,7 @@ function verifyEmail(email) {
     })
     .catch(error => {
       console.log(error.response.data);
-  });
+    });
 }
 
 // console.log(`\x1b[32mValid emails: ${validEmails.join('\n')}`);
